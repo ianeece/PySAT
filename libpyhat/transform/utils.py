675d241f481cff94634fe27f3ee832ca89420a91
Ryan <rbanderson@usgs.gov>
2019-08-13 14:14:05 -0700

    Update utils.py

--- libpyhat/transform/utils.py
+++ libpyhat/transform/utils.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May 10 16:11:37 2016
 
-@author: rbanderson
-"""
 import numpy as np
 from pandas import Series
 from scipy import signal
-
-import libpyhat.transform.baseline_code.watrous as watrous
 import numpy
 
 
@@ -108,65 +102,6 @@ def sigma_clip(data, sigma_clip=3.0, n_iter=2.0):
     return sig, mean
 
 
-"""
-Created on Tue Nov 11 18:29:29 2014
-This function is used to denoise a chemcam spectrum.
-Based on the function "denoise_spectrum.pro" in IDL
-Translated to Python by Ryan Anderson Nov 2014. Modified so that the denoised
-spectrum, and the removed noise are returned.
-@author: rbanderson
-"""
-
-
-def ccam_denoise(sp_in, sig = 3, n_iter = 4):
-    """
-    Denoises a chemcam spectrum. Based on the function "denoise_spectrum.pro" in IDL
-
-    Parameters
-    ----------
-    sp_in : ndarray
-        Array of spectrum data
-
-    sig : int
-        Unknown
-
-    n_iter : int
-        Number of iterations to refine the noise removal
-
-    Returns
-    -------
-    : float
-        The denoised spectrum
-
-    : float
-        Removed noise
-    """
-
-    s = len(sp_in)
-    lv = int(numpy.log(s) / numpy.log(2)) - 1
-    ws = watrous.watrous(sp_in, lv)
-    ws1 = ws
-
-    for i in range(lv - 2):
-        b = get_noise(ws[:, i], n_iter=n_iter)
-        tmp = ws[:, i]
-        ou = numpy.where(abs(tmp) < sig * b)
-        nou = len(tmp[ou])
-
-        if nou > 0:
-            tmp[ou] = 0
-
-        ws1[:, i] = tmp
-
-    return numpy.sum(ws1, axis=1), sp_in - numpy.sum(ws1, axis=1)
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 27 13:07:07 2016
-
-@author: rbanderson
-"""
 
 def meancenter(df, col, previous_mean=None):
     """

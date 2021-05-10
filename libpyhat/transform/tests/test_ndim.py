9483e10a0935db8c25a4277763f64dc35b25d5a0
Travis Gabriel <tgabriel@usgs.gov>
2021-05-10 13:52:11 -0700

    Fixed function call

--- libpyhat/transform/tests/test_ndim.py
+++ libpyhat/transform/tests/test_ndim.py
@@ -2,7 +2,7 @@ import conftest
 import numpy as np
 
 def test_ndim():
-    np.testing.assert_array_almost_equal(n_dim(0), ([[[1, 1, 1, 1, 1],
+    np.testing.assert_array_almost_equal(conftest.n_dim(0), ([[[1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1],

94d24d638a539b6282eade45c57c618acf6d8200
Ryan <rbanderson@usgs.gov>
2021-06-28 13:53:35 -0700

    Include predictions in LIBS test data

--- libpyhat/regression/tests/test_sm.py
+++ libpyhat/regression/tests/test_sm.py
@@ -5,33 +5,21 @@ import libpyhat.regression.sm as sm
 import libpyhat.regression.regression as reg
 
 def test_sm_blend():
-    np.random.seed(1)
     df = pd.read_csv(get_path('test_data.csv'), header=[0, 1])
-    x = df['wvl']
-    y = df[('comp','SiO2')]
 
-    model1 = reg.regression(method=['PLS'], params=[{'n_components': 3, 'scale': False}])
-    model1.fit(x, y)
-    df[('predict','model1')] = model1.predict(x)
-
-    model2 = reg.regression(method=['PLS'], params=[{'n_components': 5, 'scale': False}])
-    model2.fit(x, y)
-    df[('predict','model2')] = model2.predict(x)
-
-    model3 = reg.regression(method=['PLS'], params=[{'n_components': 4, 'scale': False}])
-    model3.fit(x, y)
-    df[('predict','model3')] = model3.predict(x)
-
-    predictions = [df[('predict','model2')],df[('predict','model1')],df[('predict','model3')],df[('predict','model1')]]
+    predictions = [df[('predict','low')],df[('predict','mid')],df[('predict','high')],df[('predict','full')]]
 
     blendranges = [[-9999,30],[20,60],[50,9999]]
     sm_obj = sm.sm(blendranges, random_seed=0)
     blended_predictions = sm_obj.do_blend(np.array(predictions)) #without optimization
     rmse = np.sqrt(np.average((blended_predictions - df[('comp', 'SiO2')]) ** 2))
-    np.testing.assert_almost_equal(rmse, 12.703434300128926, decimal=5)
+    np.testing.assert_almost_equal(rmse, 12.693586299818989, decimal=5)
 
+    #re-initialize sm_obj to make sure it behaves
+    blendranges = [[-9999, 30], [20, 60], [50, 9999]]
+    sm_obj = sm.sm(blendranges, random_seed=0)
     blended_predictions = sm_obj.do_blend(np.array(predictions),truevals=np.array(df[('comp','SiO2')]), verbose=False) #with optimization
     rmse = np.sqrt(np.average((blended_predictions-df[('comp','SiO2')])**2))
-    expected_blendranges = [-9999., 35.21056413, 50.44074802, 60.25692472, 120.39265394, 9999.]
-    np.testing.assert_almost_equal(rmse, 9.89378907900936, decimal=5)
+    expected_blendranges = [-9999., 24.0502605, 31.98075128, 53.9562582, 65.98663281, 9999.]
+    np.testing.assert_almost_equal(rmse, 12.578145384007286, decimal=5)
     np.testing.assert_allclose(expected_blendranges,np.sort(sm_obj.blendranges),rtol=1e-5)

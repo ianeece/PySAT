d6ede3b80f4729591b8ecf08d7b72a791fc63ccc
Ryan <rbanderson@usgs.gov>
2021-07-09 13:57:31 -0700

    Update deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,5 +2,6 @@ echo "Python version: $1"
 conda deactivate
 conda install conda-build anaconda-client
 conda config --set anaconda_upload yes
-conda config --env --add channels conda-forge usgs-astrogeology
+conda config --env --add channels conda-forge 
+conda config --env --add channels usgs-astrogeology
 conda build --token $CONDA_UPLOAD_TOKEN --python $1 recipe

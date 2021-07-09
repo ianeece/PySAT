331b99356943e57517d0eaf36336f9f08192ab3c
Ryan <rbanderson@usgs.gov>
2021-07-09 15:55:55 -0700

    Delete deployment directory
    This should be taken care of in GitHub actions now.

--- deployment/deploy.ps1
+++
@@ -1,5 +0,0 @@
-conda deactivate;
-conda install conda-build anaconda-client;
-conda config --set anaconda_upload yes;
-Write-Host "Python Version: $args";
-conda build --token %CONDA_UPLOAD_TOKEN% --python $args -c usgs-astrogeology recipe;
--- deployment/deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "Python version: $1"
-conda deactivate
-conda install conda-build anaconda-client
-conda config --set anaconda_upload yes
-conda config --env --add channels conda-forge 
-conda config --env --add channels usgs-astrogeology
-conda build --token $CONDA_UPLOAD_TOKEN --python $1 recipe

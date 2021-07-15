65d1a7e38825fdf83a00fd1b5e49e3158e1e7f52
Ryan <rbanderson@usgs.gov>
2021-07-15 14:55:06 -0700

    Update __init__.py

--- libpyhat/__init__.py
+++ libpyhat/__init__.py
@@ -1,13 +1,16 @@
 __version__ = "0.1.10"
 
 import libpyhat
-from libpyhat import derived
-from libpyhat import transform
-from libpyhat import regression
-from libpyhat import data
-from libpyhat import examples
 from libpyhat import analytics
+from libpyhat import clustering
+from libpyhat import data
+from libpyhat import derived
+from libpyhat import emi
+from libpyhat import examples
 from libpyhat import pyhat_io
+from libpyhat import regression
+from libpyhat import transform
+from libpyhat import utils
 from libpyhat.data.spectra import Spectra
 from libpyhat.data.spectrum import Spectrum
 from libpyhat.transform import continuum
